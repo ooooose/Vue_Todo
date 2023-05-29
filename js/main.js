b249c6e8cc28c92211088fb518c16e021cfa2f5a
ooooose <yuuki.oose1990@gmail.com>
2023-05-29 17:00:33 +0900

    [Add]todosの実装

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 const app = Vue.createApp({
     data: () => ({
-
+        newItem: '',
+        todos: []
     }),
     methods: {
         addItem: function(event) {
-            console.log('clicked!');
+            if(this.newItem === '') return
+            let todo = {
+                item: this.newItem,
+                isDone: false
+            }
+            this.todos.push(todo)
+            this.newItem = ''
         }
     }
 })
