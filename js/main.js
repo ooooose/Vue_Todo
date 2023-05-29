267d14b1a091482c3c8396810037ab82c7608338
ooooose <yuuki.oose1990@gmail.com>
2023-05-29 16:51:36 +0900

    [Add]v-onメソッドの実装

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 const app = Vue.createApp({
     data: () => ({
 
-    })
+    }),
+    methods: {
+        addItem: function(event) {
+            console.log('clicked!');
+        }
+    }
 })
 app.mount('#app')
