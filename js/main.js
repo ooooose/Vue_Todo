8e52c268e24292f53b6c7ef7c23c1560f08f0f72
ooooose <yuuki.oose1990@gmail.com>
2023-05-29 17:11:54 +0900

    [Add]Deleteボタンの実装

--- js/main.js
+++ js/main.js
@@ -12,6 +12,9 @@ const app = Vue.createApp({
             }
             this.todos.push(todo)
             this.newItem = ''
+        },
+        deleteItem: function(index) {
+            this.todos.splice(index, 1)
         }
     }
 })
